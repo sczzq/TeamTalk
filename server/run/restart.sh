2d276cf63753fbe0b5c4a5ddc5f1cd294aefeb77
sczzq <ziqiang_free@163.com>
2018-04-11 11:51:48 +0800

    fix syntax of shell.

--- server/run/restart.sh
+++ server/run/restart.sh
@@ -3,31 +3,31 @@
 
 
 function restart() {
-    cd $1
-    if [ ! -e *.conf  ]
-    then
-        echo "no config file"
-        return
-    fi
+	cd $1
+	if [ ! -e *.conf  ]
+	then
+		echo "no config file"
+		return
+	fi
 
-    if [ -e server.pid  ]; then
-        pid=`cat server.pid`
-        echo "kill pid=$pid"
-        kill $pid
-        while true
-        do
-            oldpid=`ps -ef|grep $1|grep $pid`;
-            if [ $oldpid" " == $pid" " ]; then
-                echo $oldpid" "$pid
-                sleep 1
-            else
-                break
-            fi
-        done
-        ../daeml ./$1
-    else 
-        ../daeml ./$1
-    fi
+	if [ -e server.pid  ]; then
+		pid=`cat server.pid`
+		echo "kill pid=$pid"
+		kill $pid
+		while true
+		do
+			oldpid=`ps -ef|grep $1|grep $pid`;
+			if [ $oldpid"x" == $pid"x" ]; then
+				echo $oldpid" "$pid
+				sleep 1
+			else
+				break
+			fi
+		done
+		../daeml ./$1
+	else 
+		../daeml ./$1
+	fi
 }
 
 case $1 in
@@ -46,14 +46,17 @@ case $1 in
 	file_server)
 		restart $1
 		;;
-  push_server)
-    restart $1
-    ;;
-  db_proxy_server)
-  restart $1
-  ;;
+	push_server)
+		restart $1
+		;;
+	db_proxy_server)
+		restart $1
+		;;
+	msfs)
+		restart $1
+		;;
 	*)
 		echo "Usage: "
-		echo "  ./restart.sh (login_server|msg_server|route_server|http_msg_server|file_server|push_server)"
+		echo "  ./restart.sh (login_server|msg_server|route_server|http_msg_server|file_server|push_server|db_proxy_server|msfs)"
 		;;
 esac
